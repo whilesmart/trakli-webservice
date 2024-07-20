25fc30ebddc1fb427ae0c4e118924f92883cbe9d
fenn-cs <fenn25.fn@gmail.com>
2024-07-20 17:03:58 +0100

    fix(routes): Ensure routes are generated and accesible
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Api\v1\GroupController;
-use App\Http\Controllers\Api\v1\PartyController;
-use App\Http\Controllers\Api\v1\TransactionCategoryController;
-use App\Http\Controllers\Api\v1\WalletController;
-use App\Http\Controllers\Api\v1\TransactionController;
+use App\Http\Controllers\API\v1\GroupController;
+use App\Http\Controllers\API\v1\PartyController;
+use App\Http\Controllers\API\v1\TransactionCategoryController;
+use App\Http\Controllers\API\v1\WalletController;
+use App\Http\Controllers\API\v1\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,16 +30,7 @@ Route::prefix('v1')->group(function () {
     // Auth routes
     // Resource routes
     Route::apiResource('groups', GroupController::class);
-    Route::prefix('income-categories')->group(function () {
-        Route::apiResource('', TransactionCategoryController::class)->only([
-            'index', 'store', 'show', 'update', 'destroy'
-        ]);
-    });
-    Route::prefix('expense-categories')->group(function () {
-        Route::apiResource('', TransactionCategoryController::class)->only([
-            'index', 'store', 'show', 'update', 'destroy'
-        ]);
-    });
+    Route::apiResource('categories', TransactionCategoryController::class);
     Route::apiResource('parties', PartyController::class);
     Route::apiResource('wallets', WalletController::class);
     Route::apiResource('transactions', TransactionController::class);
